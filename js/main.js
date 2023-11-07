c2aebb43acde610e812a14cf209c52a0a0ab3621
abdurahim <86982819+abbassiriyus@users.noreply.github.com>
2023-11-07 12:25:06 +0500

    salom
    juda yaxshi atyyor sayt

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function iris(){
+document.querySelector(".menu").style = "right: 0px"
+}
+function yopil(){
+document.querySelector(".menu").style = "right: -700px"
+}
+function och(){
+document.querySelector(".menu123").style = "right: 0px"
+}
+function yopil(){
+document.querySelector(".menu123").style = "right: -700px"
+}
+// // /function ochiq(){
+// docu//ment.querySelector(".menu").style = "right: 0px"
+// }/
+// function yopish(){
+// document.querySelector(".menu").style = "right: -700px"
+// }
---
+++ js/main2.js
@@ -0,0 +1,13 @@
+function och(){
+    document.querySelector(".menu123").style = "right: 0px"
+    }
+    function yopil(){
+    document.querySelector(".menu123").style = "right: -500px"
+    }
+
+    function iris(){
+        document.querySelector(".m_x").style = "right: 0px"
+        }
+        function yopil(){
+        document.querySelector(".m_x").style = "right: -500px"
+        }
